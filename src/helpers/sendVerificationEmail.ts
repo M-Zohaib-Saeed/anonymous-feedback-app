5660150acf224cc7c5feda01efc17c47fc24c14b
M-Zohaib-Saeed <mianzohaibsaeed786@gmail.com>
2026-07-25 09:37:40 +0500

    - Added a new API route for user sign-up that handles user registration and email verification. - Integrated bcryptjs for password hashing. - Created a helper function to send verification emails using the Resend service. - Added a new type definition for API responses..

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,28 @@
+import {resend} from '@/lib/resend';
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from '@/types/ApiResponse';
+
+
+
+export async function  sendVerificationEmail(
+    email: string,
+    username: string,
+    verifyCode: string
+
+) : Promise<ApiResponse> {
+    try {
+      await resend.emails.send({
+      from: 'dev@hiteshchoudhary.com',
+      to: email,
+      subject: 'Anonymous feedback Verification Code',
+      react: VerificationEmail({ username, otp: verifyCode }),
+      });
+
+      return {success : true, message: 'verivication Email send successfuly'}
+
+    } catch (emailError) {
+
+       console.error("Error sending Verification Email", emailError)
+       return {success : false, message: 'failded to send verivication Email'}
+    }
+}
